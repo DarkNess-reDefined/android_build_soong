83a82fc0f34601b37a7e75d70c761cc7d2299583
DarkKnight6499 <yazad.madan@gmail.com>
2017-09-21 13:39:59 +0000

    DND-ify rest of the things

--- android/variable.go
+++ android/variable.go
@@ -20,7 +20,7 @@ import (
 	"runtime"
 	"strings"
 
-	"lineage/soong/android"
+	"dnd/soong/android"
 
 	"github.com/google/blueprint/proptools"
 )
@@ -88,8 +88,8 @@ type variableProperties struct {
 			Cppflags []string
 		}
 
-		// include Lineage variables
-		*android.Product_variables
+		// include DND variables
+		Dnd android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -156,8 +156,8 @@ type productVariables struct {
 
 	Override_rs_driver *string `json:",omitempty"`
 
-	// include Lineage variables
-	*android.ProductVariables
+	// include DND variables
+	Dnd android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
